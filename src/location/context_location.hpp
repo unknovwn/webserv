8d65615d27f02ba1ed075bf061b3df9967437363
bmicheal77 <bmicheal@student.21-school.ru>
2021-03-31 21:51:16 +0300

    hurminette fixed

--- src/location/context_location.hpp
+++ src/location/context_location.hpp
@@ -4,8 +4,7 @@
 #include <string>
 #include <vector>
 
-typedef enum
-{
+typedef enum {
   GET,
   HEAD,
   POST,
@@ -18,20 +17,19 @@ typedef enum
 } http_method;
 
 class location {
-
   std::string               uri_;
   std::string               root_;
   bool                      autoindex_;
   std::vector<http_method>  methods_;
 
  public:
-
   const std::string&              GetUri() const;
   void                            SetUri(const std::string &uri);
   bool                            GetAutoindex() const;
   void                            SetAutoindex(bool autoindex);
   const std::vector<http_method>& GetMethods() const;
-  void                            SetMethods(const std::vector<http_method> &methods);
+  void                            SetMethods(const std::vector<http_method>
+                                    &methods);
   const std::string&              GetRoot() const;
   void                            SetRoot(const std::string &root);
 };
