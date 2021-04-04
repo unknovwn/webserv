477f43ad819d4aa8a03b8c4341d67929a0ca6983
bmicheal77 <bmicheal@student.21-school.ru>
2021-04-04 18:01:04 +0300

    request fixed

--- src/request/request.cpp
+++ src/request/request.cpp
@@ -1,1 +1,30 @@
 #include "request.hpp"
+
+//============================== METHOD ========================================
+const std::string &Request::GetMethod() const {
+  return method_;
+}
+void Request::SetMethod(const std::string &method) {
+  method_ = method;
+}
+//============================== PATH ==========================================
+const std::string &Request::GetPath() const {
+  return path_;
+}
+void Request::SetPath(const std::string &path) {
+  path_ = path;
+}
+//============================== PROTOCOL ======================================
+const std::string &Request::GetAProtocol() const {
+  return protocol_;
+}
+void Request::SetAProtocol(const std::string &a_protocol) {
+  protocol_ = a_protocol;
+}
+//============================== HEADERS =======================================
+std::map<std::string, std::string> &Request::GetHeaders() {
+  return headers_;
+}
+//void Request::SetHeaders(const std::map<std::string, std::string> &headers) {
+//  headers_ = headers;
+//}
--- src/request/request.hpp
+++ src/request/request.hpp
@@ -1,14 +1,24 @@
 #pragma once
 
+#define map__ std::map<std::string, std::string>
 #include <string>
 #include <map>
 
+using string = std::string;
+
 class Request{
 
-  std::string                        method_;
-  std::string                         path_;
-  std::string                         protocol_;
-  std::map<std::string, std::string>  headers_;
+  string                 method_;
+  string                 path_;
+  string                 protocol_;
+  map__                  headers_;
  public:
-
+  const std::string&     GetMethod() const;
+  void                   SetMethod(const std::string &method);
+  const std::string&     GetPath() const;
+  void                   SetPath(const std::string &path);
+  const std::string&     GetAProtocol() const;
+  void                   SetAProtocol(const std::string &a_protocol);
+  map__&                 GetHeaders();
+//  void                   SetHeaders(const map__ &headers);
 };
