0af425e6f81f8341baf068943d7d00c0a051fa26
bmicheal77 <bmicheal@student.21-school.ru>
2021-04-06 21:46:01 +0300

    small fixes

--- src/error_pages/error_pages.cpp
+++ src/error_pages/error_pages.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <map>
-#include <string>
 
 #define T '\t'
 
 void    generate_error_page(size_t error_code) {
-  std::ofstream out ("test.html");
+  std::ofstream out ("error.html");
   static const std::map<int, std::string> my = {
 	  {400, "Bad Request"},
 	  {403, "Forbidden"},
@@ -17,8 +16,11 @@ void    generate_error_page(size_t error_code) {
 	  {500, "Internal Server Error"}};
 
   const auto i = my.find(error_code);
-  if (i == my.end())
-    return ;
+  if (i == my.end()) {
+    out.close();
+    std::remove("error.html");
+    return;
+  }
   out << "<!DOCTYPE html>"                                 << std::endl;
   out << "<html>"                                          << std::endl;
   out << T << "<title>"        << (*i).first << "</title>" << std::endl;
@@ -27,14 +29,9 @@ void    generate_error_page(size_t error_code) {
   out << T << T << T << "<h1 align=\"center\">";
   out << (*i).first << " "  << (*i).second;
   out << "</h1>"                                           << std::endl;
+  out << T << T << T << "<hr>"                             << std::endl;
   out << T << T << "</div>"                                << std::endl;
   out << T << "</body>"                                    << std::endl;
   out << "</html>"                                         << std::endl;
   out.close();
-}
-
-
-int main() {
-  generate_error_page(400);
-  return 0;
 }
