64a5ef7b0d820abde25aa6dbc6a9820701df87aa
bmicheal77 <bmicheal@student.21-school.ru>
2021-04-06 20:50:15 +0300

    Error pages generator added

---
+++ src/error_pages/error_pages.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+
+#define T '\t'
+
+void    generate_error_page(size_t error_code) {
+  std::ofstream out ("test.html");
+  static const std::map<int, std::string> my = {
+	  {400, "Bad Request"},
+	  {403, "Forbidden"},
+	  {404, "Not Found"},
+	  {405, "Method Not Allowed"},
+	  {410, "Gone"},
+	  {413, "Payload Too Large"},
+	  {500, "Internal Server Error"}};
+
+  const auto i = my.find(error_code);
+  if (i == my.end())
+    return ;
+  out << "<!DOCTYPE html>"                                 << std::endl;
+  out << "<html>"                                          << std::endl;
+  out << T << "<title>"        << (*i).first << "</title>" << std::endl;
+  out << T << "<body>"                                     << std::endl;
+  out << T << T << "<div>"                                 << std::endl;
+  out << T << T << T << "<h1 align=\"center\">";
+  out << (*i).first << " "  << (*i).second;
+  out << "</h1>"                                           << std::endl;
+  out << T << T << "</div>"                                << std::endl;
+  out << T << "</body>"                                    << std::endl;
+  out << "</html>"                                         << std::endl;
+  out.close();
+}
+
+
+int main() {
+  generate_error_page(400);
+  return 0;
+}
