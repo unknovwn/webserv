b6e0fe06a6c888910d54b92ce86762b15fe96085
bmicheal77 <bmicheal@student.21-school.ru>
2021-04-07 17:27:08 +0300

    root identification added

--- src/autoindex/autoindex.cpp
+++ src/autoindex/autoindex.cpp
@@ -3,15 +3,16 @@
 #include <iostream>
 #include <fstream>
 
-bool   fileExists(const std::string file) {
+bool   fileExists(const std::string &file) {
   struct stat buff;
 
   return (stat(file.c_str(), &buff) == 0);
 }
 
-void    print_dir(const std::string &path) {
+void    print_dir(std::string &path, std::string &root) {
   DIR *dir;
   struct dirent *entry;
+
 
   dir = opendir(path.c_str());   // достаем из string - char *
   if (!dir) {
@@ -19,13 +20,22 @@ void    print_dir(const std::string &path) {
     return;
   }
 
+  if (path.back() != '/')   // if u put PATH without end '/'
+    path += '/';
+  if (root.back() != '/')
+    root += '/';
   std::ofstream out("autoindex.html");
   out << "<!DOCTYPE html>"                         << std::endl;
   out << "<html>"                                  << std::endl;
   out << "<title> Index of " << path << "</title>" << std::endl;
   out << "<body>"                                  << std::endl;
   out << "<div>"                                   << std::endl;
-  out << "<h1>Index of /</h1>"                     << std::endl;
+  out << "<h1>Index of ";
+
+  size_t found = path.find(root);
+  path.erase(found, root.length() - 1);
+  out << path;
+  out << "</h1>" << std::endl;
   out << "<hr>"                                    << std::endl;
   while ((entry = readdir(dir)) != NULL) {
     out << "<p><a href=\"" << path + "/" + entry->d_name << "\">";
@@ -43,8 +53,12 @@ void    print_dir(const std::string &path) {
 }
 
 int main() {
+
+  std::string path_("/Users/bmicheal/Projects/webserv/src/autoindex/root/");
+  std::string root_("/Users/bmicheal/Projects/webserv/src/autoindex/root");
+
   if (fileExists("index.php") == false)
-    print_dir("/Users/bmicheal/Projects/webserv/src/autoindex");
+    print_dir(path_, root_);
   else
     std::cout << "index.php Exists" << std::endl;
 }
